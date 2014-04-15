f79c2251ad5374c63e3842bbae1740bd84b3bbae
Lukas Pfannschmidt <mirek.1337@gmail.com>
2014-04-15 16:38:46 +0200

    SequenzImport implementiert Objekte für Sequenzen und ProgrammPunkte.

---
+++ ablauf.py
@@ -0,0 +1,40 @@
+import glob
+import json
+
+
+class ProgramPart:
+    targetTemp = 0
+    time = 0
+    def __init__(self, temp, time):
+        self.targetTemp = temp
+        self.time = time
+
+
+class Sequence:
+    name = ""
+    programs = []
+
+    def __init__(self,name,programs):
+        self.name = name
+        self.programs = programs
+
+
+def importSequences():
+    """
+    Importiert Sequenzen aus dem Unterordner /sequences/
+
+    :rtype : [Sequence]
+    :return: Liste von Sequence Objekten
+    """
+    sequences = []
+    for filename in glob.iglob("./sequences/*.seq"):
+        sequencefile = open(filename)
+        data = json.load(sequencefile)
+        programs = []
+        for programPartObject in data["Sequenz"]:
+            part = ProgramPart(programPartObject["Temp"],programPartObject["Time"])
+            programs.append(part)
+        newSequence = Sequence(data["Name"],programs)
+        sequences.append(newSequence)
+    return sequences
+
