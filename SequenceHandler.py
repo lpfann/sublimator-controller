d12ef515af8e05cae8cade353b122315fa8e1c12
Lukas Pfannschmidt <mirek.1337@gmail.com>
2014-04-16 09:53:32 +0200

    Funktion zur Speicherung von Sequenzen implementiert

--- SequenceHandler.py
+++ SequenceHandler.py
@@ -5,6 +5,7 @@ import json
 class ProgramPart:
     targetTemp = 0
     time = 0
+
     def __init__(self, temp, time):
         self.targetTemp = temp
         self.time = time
@@ -14,9 +15,14 @@ class Sequence:
     name = ""
     programs = []
 
-    def __init__(self,name,programs):
+    def __init__(self, name, programs):
         self.name = name
         self.programs = programs
+
+
+
+def jdefault(o):
+    return o.__dict__
 
 
 def importSequences():
@@ -28,14 +34,14 @@ def importSequences():
     """
     sequences = []
     for filename in glob.iglob("./sequences/*.seq"):
-        sequencefile = open(filename)
-        data = json.load(sequencefile)
-        programs = []
-        for programPartObject in data["Sequenz"]:
-            part = ProgramPart(programPartObject["Temp"],programPartObject["Time"])
-            programs.append(part)
-        newSequence = Sequence(data["Name"],programs)
-        sequences.append(newSequence)
+        with open(filename) as sequencefile:
+            data = json.load(sequencefile)
+            programs = []
+            for programPartObject in data["programs"]:
+                part = ProgramPart(programPartObject["targetTemp"], programPartObject["time"])
+                programs.append(part)
+            newSequence = Sequence(data["name"], programs)
+            sequences.append(newSequence)
     return sequences
 
 
@@ -44,4 +50,11 @@ def saveSequenceToFile(sequence):
     Speichert Sequenz im /sequence/ Ordner
     :param sequence: Sequence Object
     """
-    pass
+    name = sequence.name
+    fileExisting = glob.glob("./sequences/"+name+".seq")
+    if len(fileExisting) > 0:
+        print("File" + name + "already existing")
+    else:
+        with open("./sequences/"+name+".seq", 'w', encoding="UTF-8") as f:
+            json.dump(sequence, f, default=jdefault, indent=2)
+
