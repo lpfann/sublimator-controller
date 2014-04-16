03aea8173f3e9e820daa9c878c4a5b3e103e01c2
Dominik Gründing <dgruending@techfak.uni-bielefeld.de>
2014-04-16 11:46:46 +0200

    Temperatur-Debugging

--- hardwareAdapter.py
+++ hardwareAdapter.py
@@ -39,7 +39,7 @@ class hardwareAdapter:
     def getTemperatureCooling(self):
         value=0
         value += self.spi.read(0)
-        cool = value/10
+        cool = value
         print "Kuehlung Bit: %d" %(cool)
         temperature=((cool * 2.5043) / (4096 * 4.7)- 0.14993)*100
         return round(temperature,2)
@@ -47,7 +47,7 @@ class hardwareAdapter:
     def getTemperatureHeating(self):
         value=0
         value += self.spi.read(2)
-        print "Heizung Bit: %d" % (value/10)
+        print "Heizung Bit: %d" % (value)
         heat = (value+.0001)/10000
         temperature=3.606 * (heat * heat) + 128.58 * heat - 242.86
         return round(temperature,2)
