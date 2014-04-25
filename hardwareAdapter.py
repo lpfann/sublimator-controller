c26c18dcd353d4d981b5cc93c6bbf0781a7f7cf1
Dominik Gründing <dgruending@techfak.uni-bielefeld.de>
2014-04-25 09:58:03 +0200

    Testzwecke

--- hardwareAdapter.py
+++ hardwareAdapter.py
@@ -5,13 +5,6 @@ import atexit
 import MCP3208
 import time
 
-#Display Steuerpins(unsicher/ungetestet)
-DOG_SI=21
-DOG_CLK=23
-DOG_A0=24
-DOG_CSB=26
-DOG_RS=19
-
 #Heiz- und Kuehlelemente Pins
 HEAT=18
 COOL=16
@@ -24,11 +17,6 @@ class hardwareAdapter:
         GPIO.setwarnings(False)
         GPIO.setup(HEAT, GPIO.OUT)
         GPIO.setup(COOL, GPIO.OUT)
-        GPIO.setup(DOG_A0,GPIO.OUT)
-        GPIO.setup(DOG_CLK,GPIO.OUT)
-        GPIO.setup(DOG_CSB,GPIO.OUT)
-        GPIO.setup(DOG_RS,GPIO.OUT)
-        GPIO.setup(DOG_SI,GPIO.OUT)
 
         # Initialisierung des A/D Wandlers,
         self.spi = MCP3208.MCP3208(0)
@@ -40,58 +28,6 @@ class hardwareAdapter:
         GPIO.output(HEAT, False)
         GPIO.output(COOL, False)
         GPIO.cleanup()
-
-    def initLcd(self):
-        GPIO.output(DOG_RS, 0)
-        time.sleep(0.01)
-        GPIO.output(DOG_RS, 1)
-        time.sleep(0.01)
-        init_seq = [ 0x40, 0xA1, 0xC0, 0xA6, 0xA2, 0x2F, 0xF8, 0x00, 0x27, 0x81, 0x16, 0xAC, 0x00, 0xAF ]
-        self.sendCmdSeq(init_seq)
-
-    def setPos(self,page,column):
-        self.sendCmd(0xB0 + page)
-        self.sendCmd(0x10 + ((column&0xF0)>>4))
-        self.sendCmd(0x00 + (column&0x0F))
-
-    def clearLcd(self):
-        for i in range(8):
-            self.setPos(i, 0)
-            self.sendDataSeq([0x00]*128)
-
-    def sendByte(self,data):
-        if type(data)==type('a'):
-            data=ord(data)
-        GPIO.output(DOG_CSB,0)
-        for i in range(8):
-            if data&128 > 0:
-                GPIO.output(DOG_SI, 1)
-            else:
-                GPIO.output(DOG_SI, 0)
-            data = data<<1
-            GPIO.output(DOG_CLK, 0)
-            time.sleep(0.00001)
-            GPIO.output(DOG_CLK, 1)
-            time.sleep(0.00001)
-        GPIO.output(DOG_CSB, 1)
-        time.sleep(0.01)
-
-    def sendCmd(self,cmd):
-        GPIO.output(DOG_A0, 0)
-        self.sendByte(cmd)
-        time.sleep(0.001)
-
-    def sendCmdSeq(self,cmdSeq):
-        for cmd in cmdSeq:
-            self.sendCmd(cmd)
-
-    def sendData(self,data):
-        GPIO.output(DOG_A0,1)
-        self.sendByte(data)
-
-    def sendDataSeq(self,dataSeq):
-        for data in dataSeq:
-            self.sendData(data)
 
     def start(self):
         self.heatingON()
@@ -120,23 +56,20 @@ class hardwareAdapter:
         temperature=3.606 * (heat * heat) + 128.58 * heat - 242.86
         return round(temperature,2)
 
-
-    def display(self,value):
-        a=1
-
 if __name__ == '__main__':
     hA=hardwareAdapter()
-    hA.initLcd()
-    time.sleep(5)
-    for i in range(8):
-        data = 1
-        for j in range(4):
-            hA.setPos(i, 0)
-            hA.sendDataSeq([data]*128)
-            data += pow(4, j+1)
-    for i in range(64):
-        for j in range(8):
-            hA.setPos(j, i*2)
-            hA.sendDataSeq([ 0xFF, 0x00 ])
-    hA.clearLcd()
-    print "Finished"
+    hA.start()
+    temp,cool=0
+    while True:
+        temp=hA.getTemperatureHeating()
+        cool=hA.getTemperatureCooling()
+        print "Temperatur: %2f  Kuehlung: %2f" %(round(temp,2),round(cool,2))
+        if temp>160:
+            hA.heatingOFF()
+        elif temp<155:
+            hA.heatingON()
+        if cool<6:
+            hA.coolingOFF()
+        elif cool > 9:
+            hA.coolingON()
+        time.sleep(3)
