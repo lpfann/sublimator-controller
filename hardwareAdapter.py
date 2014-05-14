01075adb5488aa9003f07e3be5dccfc799dff608
Dominik Gründing <dgruending@techfak.uni-bielefeld.de>
2014-05-14 11:21:56 +0200

    Pin-Status Abfrage

--- hardwareAdapter.py
+++ hardwareAdapter.py
@@ -78,6 +78,6 @@ if __name__ == '__main__':
         time.sleep(3)"""
     for i in [3,5,7,8,10,11,12,13,15,16,18,19,21,22,23,24,26]:
         func=GPIO.gpio_function(i)
-        #pin=GPIO.input(i)
-        pin=0
-        print "Pin: "+i+" PinFunktion: " + func + " Status: "+pin
+        if func==GPIO.IN or func==GPIO.OUT:
+            pin=GPIO.input(i)
+        print "Pin: %d PinFunktion: " + func + " Status: %d" %(i,pin)
